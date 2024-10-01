2009f82af71c152b04f00eec414aaad7a93fbba6
ElioAl <elio_a@icloud.com>
2024-10-01 18:51:52 -0300

    Completed About Us Feature

--- app/src/main/java/ca/unb/mobiledev/saferide/Aboutus.kt
+++ app/src/main/java/ca/unb/mobiledev/saferide/Aboutus.kt
@@ -1,6 +1,7 @@
 package ca.unb.mobiledev.saferide
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +16,12 @@ class Aboutus : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val backID =findViewById<Button>(R.id.backButton)
+
+        backID.setOnClickListener {
+            finish()
         }
     }
 }
