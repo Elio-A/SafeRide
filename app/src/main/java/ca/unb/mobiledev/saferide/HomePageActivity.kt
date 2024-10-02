e7aac22dbda8f67c87cc2650de081c788d04562e
Khaled <k9developments@outlook.com>
2024-10-01 18:33:32 -0800

    Multiple Changes Made
    Tried linking MainActivity with HomePageActivity

--- app/src/main/java/ca/unb/mobiledev/saferide/HomePageActivity.kt
+++ app/src/main/java/ca/unb/mobiledev/saferide/HomePageActivity.kt
@@ -1,6 +1,5 @@
 package ca.unb.mobiledev.saferide
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -8,25 +7,37 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
-
+class HomePageActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        //Moving from main to home page using the button:
-        val goToHomePageButton: Button = findViewById(R.id.next_activity)
-        goToHomePageButton.setOnClickListener{
-            val intent = Intent(this@MainActivity, HomePageActivity::class.java)
-            startActivity(intent)
+        val stationButton : Button = findViewById(R.id.pickup_stations_button) //What's wrong with this?
+        val radiusButton : Button = findViewById(R.id.radius_button)
+        val workingHourButton : Button = findViewById(R.id.working_hours_button)
+        val informationButton : Button = findViewById(R.id.information_button)
+
+        stationButton.setOnClickListener{
+            //Go to that activity
+        }
+
+        radiusButton.setOnClickListener{
+
+        }
+
+        workingHourButton.setOnClickListener{
+
+        }
+
+        informationButton.setOnClickListener{
+
         }
     }
-}//End MainActivity
+}//End HomePageActivity
