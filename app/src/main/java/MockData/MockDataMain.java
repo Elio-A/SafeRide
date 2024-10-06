94b3425fff67787bb2cbfb135e6e260852154706
ElioAl <elio_a@icloud.com>
2024-10-06 20:02:32 -0300

    bug fixes to the MockData

--- app/src/main/java/MockData/MockDataMain.java
+++ app/src/main/java/MockData/MockDataMain.java
@@ -1,10 +1,10 @@
 package MockData;
 
 public class MockDataMain {
-    User user1 = new User("Elio", "Al Alam", 3717406, "ealalam@unb.ca", "Elio");
-    User user2 = new User("Albertus", "Koesoama", 3712385, "albertus.university@unb.ca", "albert");
+    static User user1 = new User("Elio", "Al Alam", 3717406, "ealalam@unb.ca", "Elio");
+    static User user2 = new User("Albertus", "Koesoama", 3712385, "albertus.university@unb.ca", "albert");
 
-    public User isAUser(int studentNumber){
+    public static User isAUser(int studentNumber){
         if(studentNumber == user1.getStudentNumber()){
             return user1;
         } else if(studentNumber == user2.getStudentNumber()){
